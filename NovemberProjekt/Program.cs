4f8c052a73e224dc2f50bb2559ccc58f8e556a78
Teknokz <60339592+Teknokz@users.noreply.github.com>
2020-11-17 12:25:26 +0100

    Initial Commit

---
+++ NovemberProjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NovemberProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
