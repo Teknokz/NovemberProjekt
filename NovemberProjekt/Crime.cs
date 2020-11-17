a3b2d38e53b6378cda6740f20661139c298ab3a6
Teknokz <60339592+Teknokz@users.noreply.github.com>
2020-11-17 13:31:24 +0100

    Added first crime

---
+++ NovemberProjekt/Crime.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace NovemberProjekt
+{
+    public class Crime
+    {
+        
+    List<string> crimes = new List<string>()
+    {"incest",""};
+
+    List<string> description = new List<string>()
+    {"INCEST - 6 kap 7§ Den som, i annat fall än som avses förut i detta kapitel, har samlag med eget barn eller dess avkomling, döms för samlag med avkomling till fängelse i högst två år."
+    
+    };
+
+        
+
+
+    }
+}
--- NovemberProjekt/Program.cs
+++ NovemberProjekt/Program.cs
@@ -6,7 +6,10 @@ namespace NovemberProjekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+
+            Console.ReadLine();
         }
     }
 }
